62edf0039ebe8a9ae9ca58d977562ff9ee2dd19e
Lin, Cheng-Tao <mutolisp@gmail.com>
2015-08-16 05:49:11 +0800

    新增可一個個選擇物種(QCompletion 自動補齊)

--- sandbox/window.py
+++ sandbox/window.py
@@ -16,6 +16,8 @@ class Window(QWidget, Ui_Window):
         self.connect(self.butBlist, SIGNAL("clicked()"), self.browBaselist)
         self.connect(self.butSlist, SIGNAL("clicked()"), self.browSlist)
         self.connect(self.butGenerate, SIGNAL("clicked()"), self.generate)
+        self.connect(self.butAddToTree, SIGNAL("clicked()"), self.addToTree)
+        self.connect(self.butGenerateSp, SIGNAL("clicked()"), self.generateSp)
 #        self.connect(self.
 #        f = open('/tmp/log', 'w')
 #        f.write(ofile + '\n')
@@ -28,6 +30,11 @@ class Window(QWidget, Ui_Window):
         if Blist is None:
             return
         self.lineBlist.setText(Blist) 
+        completer = QCompleter()
+        self.lineSpecies.setCompleter(completer)
+        model = QStringListModel()
+        completer.setModel(model)
+        self.getCompleteData(model, str(self.lineBlist.text()))
 
     def browSlist(self):
         self.lineSlist.clear()
@@ -35,6 +42,49 @@ class Window(QWidget, Ui_Window):
         if Slist is None:
             return
         self.lineSlist.setText(Slist) 
+
+    # import data into auto-completion list
+    def getCompleteData(self, model, blist):
+        b_container = []
+        with open(blist, newline='', encoding='utf-8') as f:
+            reader = csv.reader(f, delimiter='|')
+            for r in reader:
+                # only read common names, name without author and family common name
+                # ex: 松葉蕨, Psilotum nudum, 松葉蕨科
+                b_container.append(r[2] + "," + r[3] + "," + r[1])
+        model.setStringList(b_container)
+
+    def addToTree(self):
+        item = QTreeWidgetItem()     
+        species_item = str.split(str(self.lineSpecies.text()), ',')
+        item.setText(0, species_item[2])
+        item.setText(1, species_item[1])
+        item.setText(2, species_item[0])
+        self.treeWidget.addTopLevelItem(item)
+        self.lineSpecies.clear()
+
+    def delFromTree(self):
+        #removing the QTreeItemWidget object
+        self.treeWidget.takeTopLevelItem(treeWidget.indexOfTopLevelItem(self))
+
+
+    def get_all_items(self, tree_widget):
+        all_items = []
+        root = tree_widget.invisibleRootItem()
+        child_count = root.childCount()
+        for i in range(child_count):
+            item = root.child(i)
+            all_items.append(item.text(2))
+        return all_items
+
+    def generateSp(self):
+        temp_container = []
+        print(self.treeWidget.items)
+        tree_item = self.get_all_items(self.treeWidget)
+        f = open('/tmp/log', 'a')
+        for row in range(len(tree_item)):
+            f.write(tree_item[row]+'\n')
+        f.close()
 
     def generate(self):
         g = genlist_api.genlist()
