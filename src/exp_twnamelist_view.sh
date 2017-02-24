2101a97525919a11f5883a0d759b47bf7d6b8ab7
Lin, Cheng-Tao <mutolisp@gmail.com>
2017-02-24 14:10:07 +0800

    修正大花紫薇科名

--- src/exp_twnamelist_view.sh
+++ src/exp_twnamelist_view.sh
@@ -6,5 +6,5 @@ DB=nvdimp
 ${PGSQL} -d ${DB} -qA -t -c "SELECT * FROM nomenclature.twnamelist_floratw_view" > ../data/twnamelist.csv
 ${PGSQL} -d ${DB} -qA -t -c "SELECT * FROM nomenclature.twnamelist_view" > ../data/twnamelist_pg.csv
 ${PGSQL} -d ${DB} -qA -t -c "SELECT * FROM nomenclature.twnamelist_synonym_view" > ../data/twnamelist_pg_synonym.csv
-${PGSQL} -d ${DB} -qA -t -c "SELECT * FROM nomenclature.twbirdlist_view" | sed -e 's/I\ */I/g'> ../data/twbirdlist_2014.csv
-${PGSQL} -d ${DB} -qA -t -c "SELECT * FROM nomenclature.jp_ylist_view" > ../data/jp_ylist.csv
+#${PGSQL} -d ${DB} -qA -t -c "SELECT * FROM nomenclature.twbirdlist_view" | sed -e 's/I\ */I/g'> ../data/twbirdlist_2014.csv
+#${PGSQL} -d ${DB} -qA -t -c "SELECT * FROM nomenclature.jp_ylist_view" > ../data/jp_ylist.csv
